8e067570c25d80bb8ced6845fdf9026d1d79b404
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-11-27 17:25:19 +0900

    Feat: add UI for current directory

--- cat-img-gallery/src/components/Breadcrumb.js
+++ cat-img-gallery/src/components/Breadcrumb.js
@@ -1,1 +1,27 @@
-export default class Breadcrumb {}
+export default class Breadcrumb {
+	$target;
+	$state;
+
+	constructor({ $app, initialState }) {
+		this.$state = initialState;
+		this.$target = document.createElement('nav');
+		this.$target.className = 'Breadcrumb';
+		$app.appendChild(this.$target);
+	}
+
+	setState(nextState) {
+		this.$state = nextState;
+		this.render();
+	}
+
+	render() {
+		this.$target.innerHTML = /* html */ ` <div class="nav-item">root</div>
+			${this.state
+				.map(
+					(node, index) => /* html */ `
+                <div class="nav-item" data-index="${index}">${node.name}</div>
+            `,
+				)
+				.join('')}`;
+	}
+}
