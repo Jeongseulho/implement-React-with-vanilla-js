c27b2f6d83930f3e649d7c38045d0067d70aad74
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-12-01 15:39:02 +0900

    Feat: add click event and Router

--- Human-resource-info/src/App.js
+++ Human-resource-info/src/App.js
@@ -1,8 +1,30 @@
 import Component from './core/Component.js';
 import Header from './components/Header.js';
+// import HomePage from './components/HomePage.js';
 
 export default class App extends Component {
+	template() {
+		return /* html */ `
+			<header></header>
+			<main></main>
+		`;
+	}
+
 	mounted() {
-		new Header(this.$target);
+		const header = this.$target.querySelector('header');
+		new Header(header, {
+			changeUrl: this.changeUrl.bind(this),
+		});
+
+		const main = this.$target.querySelector('main');
+		// new HomePage(main);
+	}
+
+	changeUrl(url) {
+		window.history.pushState('', '', url);
+		const urlChange = new CustomEvent('urlchange', {
+			detail: { href: url },
+		});
+		document.dispatchEvent(urlChange);
 	}
 }
--- Human-resource-info/src/components/Header.js
+++ Human-resource-info/src/components/Header.js
@@ -3,14 +3,20 @@ import Component from '../core/Component.js';
 export default class Header extends Component {
 	template() {
 		return /* html */ `
-			<header>
 				<div class="header header_left">
 					<span class="menu_name" id="menu_home">Home</span>
 				</div>
 				<div class="header header_right">
 					<span class="menu_name" id="menu_signup">SIGN UP</span>
 				</div>
-			</header>
 		`;
+	}
+
+	setEvent() {
+		const { changeUrl } = this.$props;
+
+		this.addEvent('click', '#menu_home', () => changeUrl('/web/'));
+
+		this.addEvent('click', '#menu_signup', () => changeUrl('/web/signup'));
 	}
 }
