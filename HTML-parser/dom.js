f39f811e974de85c8f8d893b59e2484f62e3150a
Jeong seulho <wjdtmfgh@gmail.com>
2023-02-05 16:49:33 +0900

    Feat: implement parse class and test parsing

--- HTML-parser/dom.js
+++ HTML-parser/dom.js
@@ -1,3 +1,8 @@
+function assert(condition, message) {
+  if (!condition) {
+    throw new Error(message || 'Assertion failed');
+  }
+}
 export class KarlNode {
   children;
   nodeType;
@@ -35,4 +40,126 @@ export function createElement(name, attrs, children) {
     children,
     nodeDetail: new KarlElement({ tagName: name, attributes: attrs }),
   });
+}
+
+export default class HtmlParser {
+  html;
+  idx;
+  constructor(html, idx) {
+    this.html = html;
+    this.idx = idx;
+  }
+
+  getChar() {
+    return this.html[this.idx];
+  }
+
+  isStartWith(str) {
+    const charArr = Array.from(str);
+    let curIdx = this.idx;
+
+    return charArr.every((char) => {
+      if (this.html[curIdx] === char) {
+        curIdx++;
+        return true;
+      }
+      return false;
+    });
+  }
+
+  isEndOfHtml() {
+    return this.idx >= this.html.length;
+  }
+
+  *getHtmlIterator(html, start = 0) {
+    for (let i = start; i < html.length; i++) {
+      yield [i, html[i]];
+    }
+  }
+
+  consumeChar() {
+    const htmlIterator = this.getHtmlIterator(this.html, this.idx);
+    const [curIdx, curChar] = htmlIterator.next().value;
+    this.idx++;
+  }
+
+  consumeLoop(testFnc) {
+    let consumedHtml = '';
+    while (!this.isEndOfHtml() && testFnc(this.getChar())) {
+      result += this.consumeChar();
+    }
+    return consumedHtml;
+  }
+
+  consumeWhitespace() {
+    this.consumeLoop((char) => char === ' ');
+  }
+
+  parse() {
+    const nodes = this.parseNodes();
+    if (nodes.length === 1) {
+      return nodes.pop();
+    }
+    return createElement('html', {}, nodes);
+  }
+
+  parseNodes() {
+    if (this.getChar() === '<') {
+      return this.parseElement();
+    }
+    return this.parseText();
+  }
+
+  parseElement() {
+    assert(this.getChar() === '<', 'char is not <');
+    const tagName = this.parseTagName();
+    const attrs = this.parseAttrs();
+
+    assert(this.consumeChar() === '>', 'char is not >');
+    const children = this.parseNodes();
+
+    assert(this.consumeChar() === '<', 'char is not <');
+    assert(this.consumeChar() === '/', 'char is not </');
+    assert(this.parseTagName() === tagName, 'tag name is not matched');
+    assert(this.consumeChar() === '>', 'char is not >');
+    return createElement(tagName, attrs, children);
+  }
+
+  parseTagName() {
+    return this.consumeLoop((char) => {
+      return char.match(/[a-zA-Z0-9]/);
+    });
+  }
+
+  parseAttrs() {
+    let attrs = {};
+
+    while (true) {
+      this.consumeWhitespace();
+      if (this.getChar() === '>') {
+        break;
+      }
+      const { name, value } = this.parseAttr();
+      attrs[name] = value;
+    }
+    return attrs;
+  }
+  parseAttr() {
+    const name = this.parseTagName();
+    assert(this.consumeChar() === '=', 'char is not =');
+    const value = this.parseAttrValue();
+    return { name, value };
+  }
+
+  parseAttrValue() {
+    const quote = this.consumeChar();
+    assert(quote === '"' || quote === "'", 'quote is not " or \'');
+    const value = this.consumeWhile((char) => char !== quote);
+    assert(this.consumeChar() === quote, 'quote is not " or \'');
+    return value;
+  }
+
+  parseText() {
+    return createText(this.consumeWhile((char) => char !== '<'));
+  }
 }
--- HTML-parser/main.js
+++ HTML-parser/main.js
@@ -1,1 +1,4 @@
+import HtmlParser from './dom.js';
 const html = `<html><body><h1>Title</h1><div id="main" class="test"><p>Hello<em>world</em>!</p></div></body></html>`;
+const parsedHtml = new HtmlParser(html, 0);
+console.log(parsedHtml);
