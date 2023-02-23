44442dea802098cebe9034b0f452b8f83974f3aa
Jeong seulho <wjdtmfgh@gmail.com>
2023-02-23 22:07:15 +0900

    Feat: migrate js to ts and fix error

--- HTML-parser/src/main.ts
+++ HTML-parser/src/main.ts
@@ -1,4 +1,5 @@
-import HtmlParser from './dom.js';
+import HtmlParser from './dom';
 const html = `<html><body><h1>Title</h1><div id="main" class="test"><p>Hello<em>world</em>!</p></div></body></html>`;
-const parsedHtml = new HtmlParser(html, 0);
+const htmlParser = new HtmlParser(html, 0);
+const parsedHtml = htmlParser.parse();
 console.log(parsedHtml);
