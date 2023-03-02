522706b60b79afbf425e945d7a84db07645d0fd1
Jeong seulho <wjdtmfgh@gmail.com>
2023-03-02 18:36:39 +0900

    Del: del html parser

--- HTML-parser/src/main.ts
+++
@@ -1,5 +0,0 @@
-import HtmlParser from './dom';
-const html = `<html><body><h1>Title</h1><div id="main" class="test"><p>Hello<em>world</em>!</p></div></body></html>`;
-const htmlParser = new HtmlParser(html, 0);
-const parsedHtml = htmlParser.parse();
-console.log(parsedHtml);
