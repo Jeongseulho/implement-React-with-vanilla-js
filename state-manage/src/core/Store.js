f919a56b1ccd2dc20ab48b7954957a360c9ff6c3
Jeong seulho <wjdtmfgh@gmail.com>
2023-06-13 17:05:44 +0900

    edit method grammar

--- state-manage/src/core/Store.js
+++ state-manage/src/core/Store.js
@@ -1,16 +1,18 @@
 import { observable } from './observer.js';
 
 export const createStore = (reducer) => {
-
   // reducer가 실행될 때 반환하는 객체(state)를 observable로 만들어야 한다.
   const state = observable(reducer());
 
   // getState가 실제 state를 반환하는 것이 아니라 frozenState를 반환하도록 만들어야 한다.
   const frozenState = {};
-  Object.keys(state).forEach(key => {
+  Object.keys(state).forEach((key) => {
     Object.defineProperty(frozenState, key, {
-      get: () => state[key], // get만 정의하여 set을 하지 못하도록 만드는 것이다.
-    })
+      // get만 정의하여 set을 하지 못하도록 만드는 것이다.
+      get() {
+        state[key];
+      },
+    });
   });
 
   // dispatch로만 state의 값을 변경할 수 있다.
@@ -22,11 +24,10 @@ export const createStore = (reducer) => {
       if (!state[key]) continue;
       state[key] = value;
     }
-  }
+  };
 
   const getState = () => frozenState;
 
   // subscribe는 observe로 대체한다.
   return { getState, dispatch };
-
-}
+};
